dff2663e97bb52af3769e69230818b6b2541f399
MikhailU <1>
2021-06-11 18:31:05 +0300

    сделал шапку

---
+++ markup/components/header/data/data.js
@@ -0,0 +1,19 @@
+data = {
+  header: {
+    main: {
+      logo: '__static__img/assets/header/logo.png',
+      mail: 'bestcarnn@mail.ru',
+      tel: '+7 (952) 780-24-70',
+      socials: ['#', '#', '#'],
+      links: {
+        '#whatCheck': 'что мы проверяем',
+        '#whatUse': 'чем пользуемся',
+        '#services': 'услуги',
+        '#checkAuto': 'проверка автомобиля',
+        '#about': ' о компании',
+        '#whyWe': 'почему мы?',
+        '#comments': 'отзывы'
+      }
+    }
+  }
+};
