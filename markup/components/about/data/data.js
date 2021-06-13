9950ba7a77315fbb55fc3dff6cefc654e3654c95
MikhailU <1>
2021-06-13 22:27:35 +0300

    добавил попапы, допилил о компании:

--- markup/components/about/data/data.js
+++ markup/components/about/data/data.js
@@ -1,7 +1,7 @@
 data = {
   about: {
     main: {
-      photo: '__static__img/assets/about/1.jpg',
+      photo: '__static__img/assets/about/businessman.png',
     }
   }
 };
--- markup/components/services/data/data.js
+++ markup/components/services/data/data.js
@@ -5,37 +5,44 @@ data = {
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель Двигатель Двигатель Двигатель Двигатель',
-          info: 'Вы подбираете 5-7 вариантов, которые вам нравятся, и осматриваете их вместе со специалистом в течение рабочего дня. '
+          info: 'Вы подбираете 5-7 вариантов, которые вам нравятся, и осматриваете их вместе со специалистом в течение рабочего дня. ',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером'
+          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения'
+          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером'
+          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером'
+          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером'
+          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
           photo: '__static__img/assets/services/3.png',
           name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером'
+          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
+          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
       ]
     }
