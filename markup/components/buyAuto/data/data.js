eabcf63bbc25b59c2cfc37052d102e91ce1d2099
MikhailU <1>
2021-06-13 20:10:54 +0300

    сделал секцию с нами легко

---
+++ markup/components/buyAuto/data/data.js
@@ -0,0 +1,28 @@
+data = {
+  buyAuto: {
+    main: {
+      items: [
+        {
+          icon: '__static__img/assets/buyAuto/pic-1.png',
+          info: 'Даём гарантию на подобранный автомобиль 2 месяца',
+        },
+        {
+          icon: '__static__img/assets/buyAuto/pic-2.png',
+          info: 'Торгуемся с продавцом за Вас',
+        },
+        {
+          icon: '__static__img/assets/buyAuto/pic-3.png',
+          info: 'Даём гарантию на подобранный автомобиль 2 месяца',
+        },
+        {
+          icon: '__static__img/assets/buyAuto/pic-4.png',
+          info: 'Торгуемся с продавцом за Вас',
+        },
+        {
+          icon: '__static__img/assets/buyAuto/pic-5.png',
+          info: 'Даём гарантию на подобранный автомобиль 2 месяца',
+        },
+      ]
+    }
+  }
+};
