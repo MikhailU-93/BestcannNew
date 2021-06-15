e4f2acedc901274ca4bc3772e72a335216b6aa13
MikhailU <1>
2021-06-15 17:09:27 +0300

    сделал небольшие правки и добавил беки нескольким секциям

--- markup/components/services/data/data.js
+++ markup/components/services/data/data.js
@@ -20,30 +20,6 @@ data = {
           info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером Осмотр кузова на ремонты, повреждения',
           more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
-        {
-          photo: '__static__img/assets/services/3.png',
-          name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
-          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
-        },
-        {
-          photo: '__static__img/assets/services/3.png',
-          name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
-          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
-        },
-        {
-          photo: '__static__img/assets/services/3.png',
-          name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
-          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
-        },
-        {
-          photo: '__static__img/assets/services/3.png',
-          name: 'Двигатель',
-          info: 'Осмотр кузова на ремонты, повреждения, перекрасы. Проверка на ДТП. Проверка производится как толщиномером',
-          more: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
-        },
       ]
     }
   }
