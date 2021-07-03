c583ab4c529e57931d7f8be3023a327552fd1b8a
MikhailU <1>
2021-07-03 12:01:25 +0300

    подредактировал комменты

--- markup/components/comments/data/data.js
+++ markup/components/comments/data/data.js
@@ -3,28 +3,28 @@ data = {
     main: {
       items: [
         {
-          photo: ['__static__img/assets/comments/photo-1.jpg', '__static__img/assets/comments/photo-2.jpg', '__static__img/assets/comments/photo-3.png', '__static__img/assets/comments/photo-4.png'],
+          photo: '__static__img/assets/comments/photo-1.jpg',
           desc: 'Марка, модель, описание(жирным)',
           for: 'для Михаила',
           info: 'Вы подбираете 5-7 вариантов, которые вам нравятся, и осматриваете их вместе со',
           comment: 'Учитывая радость от покупки долгожданного авто не могу не оставить свой отзыв. Очень благодарна за такую профессиональную помощь, отзывчивость и терпение! Признаюсь, с учетом ограниченного бюджета и требований к будущему авто, в особенности автоматическая коробка передач, Владимир предупредил меня еще за несколько месяцев до приобретения, что задача не из легких и, возможно, придется искать долго и упорно. Сколько вопросов было мной задано за это время, как неопытным водителем, сколько объявлений просмотрено, сколько проведено осмотров. На каждый из них Владимир давал четкие и понятные ответы, подробно рассказывал о недостатках того или иного варианта. Владимир и его коллеги из Bestcarnn провели тщательный анализ всех основных технических агрегатов на предмет поломок и возможных правовых ограничений, и, убедившись в их отсутствии, я стала обладательницей Opel Astra (А-Н NВ) седан 2010 года. Мое обращение к Владимиру и Bestcarnn – было самое правильное решение!'
         },
         {
-          photo: ['__static__img/assets/comments/photo-1.jpg', '__static__img/assets/comments/photo-2.jpg', '__static__img/assets/comments/photo-3.png', '__static__img/assets/comments/photo-4.png'],
+          photo: '__static__img/assets/comments/photo-1.jpg',
           desc: 'Марка, модель, описание(жирным)',
           for: 'для Михаила',
           info: 'Вы подбираете 5-7 вариантов, которые вам нравятся, и осматриваете их вместе со',
           comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem? velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem?'
         },
         {
-          photo: ['__static__img/assets/comments/photo-1.jpg', '__static__img/assets/comments/photo-2.jpg', '__static__img/assets/comments/photo-3.png', '__static__img/assets/comments/photo-4.png'],
+          photo: '__static__img/assets/comments/photo-1.jpg',
           desc: 'Марка, модель, описание(жирным)',
           for: 'для Михаила',
           info: 'Вы подбираете 5-7 вариантов, которые вам нравятся, и осматриваете их вместе со',
           comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt veritatis asperiores dolores commodi, recusandae a expedita reprehenderit quibusdam qui! Culpa quae cumque magni quidem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ut velit mollitia impedit eos, nesciunt'
         },
         {
-          photo: ['__static__img/assets/comments/photo-1.jpg', '__static__img/assets/comments/photo-2.jpg', '__static__img/assets/comments/photo-3.png', '__static__img/assets/comments/photo-4.png'],
+          photo: '__static__img/assets/comments/photo-1.jpg',
           desc: 'Марка, модель, описание(жирным)',
           for: 'для Михаила',
           info: 'Вы подбираете 5-7 вариантов, которые вам нравятся, и осматриваете их вместе со',
