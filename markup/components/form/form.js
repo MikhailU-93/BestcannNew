e36cb60d9eb76c092252971c2e734896b7fe666c
MikhailU <1>
2021-07-05 15:11:29 +0300

    пофиксил подключение шрифтов

--- markup/components/form/form.js
+++ markup/components/form/form.js
@@ -1,1 +1,24 @@
+import $ from 'jquery';
+
+const form = $('.form_main');
+const action = form.attr('action');
+
+const brandsSelect = $('.form_select.-brands');
+const brandsOption = $('.form_option.-brands');
+
+const modelsSelect = $('.form_select.-models');
+// const modelsOption = modelsSelect.find('.form_option');
+
+brandsSelect.addClass('-active');
+modelsSelect.eq(0).addClass('-active');
+
+brandsOption.on('click', function() {
+  const selectedIndex = $(this).index();
+  //console.log(selectedIndex);
+  console.log(123);
+
+  modelsSelect.removeClass('-active');
+  modelsSelect.eq(index).addClass('-active');
+});
+
 
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -3,10 +3,11 @@
 /*
     This file can be used as entry point for webpack!
  */
+import 'components/header/header.js';
 import 'components/whatCheck/whatCheck.js';
 import 'components/whatUse/whatUse.js';
 import 'components/services/services.js';
-import 'components/header/header.js';
 import 'components/comments/comments.js';
+import 'components/form/form.js';
 
 import 'components/popup/popup.js';
